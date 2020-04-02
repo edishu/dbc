4aa78c66ff12e745657ce1d9401c553a687a6cd3
Udayan Maurya <edmauryaishu@gmail.com>
2020-04-02 01:55:23 -0400

    Deployed to firebase

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,6 +1,8 @@
 // Import important libraries
 import React, {Component} from 'react';
-import {Navbar} from 'react-bootstrap';
+import {Navbar, Nav} from 'react-bootstrap';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 import classes from "./footer.module.css";
 
@@ -9,9 +11,24 @@ class Footer extends Component {
         return (
             <div className={classes.myFooter}>
                 <Navbar bg="dark" variant="dark"  fixed="bottom" >
-                    <Navbar.Brand>
-                    Don't Break the Chain
-                    </Navbar.Brand>
+                    <Nav className="ml-auto" >
+                        <Navbar.Brand className={classes.navbarBrand}>
+                            <Nav.Link 
+                            href="https://www.linkedin.com/in/udayanmaurya" 
+                            className={classes.navLink} 
+                            target='_blank'>
+                                <LinkedInIcon fontSize='large'/>
+                            </Nav.Link>
+                        </Navbar.Brand>
+                        <Navbar.Brand className={classes.navbarBrand}>    
+                            <Nav.Link 
+                            href="https://github.com/edishu/dbc" 
+                            className={classes.navLink}
+                            target='_blank'>
+                                <GitHubIcon fontSize='large'/>
+                            </Nav.Link>
+                        </Navbar.Brand>
+                    </Nav>
                 </Navbar>
             </div>);
     }
